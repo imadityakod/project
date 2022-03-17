c3c75534f7f30b1d387f020548dad5846ef767a7
Cloud User <centos@ip-172-31-91-102.ec2.internal>
2022-03-17 05:19:26 +0000

    app done

--- src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
+++ src/test/java/com/ravi/cal/RaviCalculator/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-public class CalculatorTest {
+    public class CalculatorTest {
     @Test
     public void twoAndThreeIsFive() throws Exception {
         final long result = new Calculator().addFucn(2, 3);
